4664a61307ee5565acecb5f2fbbd8f9f1fb93b26
L3P3 <dev@l3p3.de>
2023-08-10 22:08:48 +0200

    feature: json schema enforcement

--- src/parsers/json.js
+++ src/parsers/json.js
@@ -1,4 +1,9 @@
 export default async function parseJSON(src, path) {
-	return JSON.parse(src);
-	// that was simple, wasn't it?
+	const parsed = JSON.parse(src);
+	// that was simple, wasn't it? oh wait, we need to annoy people by enforcing a schema
+	if (parsed.$schema !== 'https://l3p3.de/shr/schema/lui-templates.intermediary.json') {
+		throw new Error(`Invalid schema in file: ${path}`);
+	}
+	// TODO validate the schema? nah... later, maybe...
+	return parsed;
 }
